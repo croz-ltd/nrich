b8d8e05b74b33e71927fea337486741bf9b092e3
agrancaric <agrancaric@croz.net>
2020-05-19 12:47:55 +0200

    initial test search configuration

---
+++ nrich-search/src/test/java/net/croz/nrich/search/SearchConfigurationTestConfiguration.java
@@ -0,0 +1,8 @@
+package net.croz.nrich.search;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class SearchConfigurationTestConfiguration {
+
+}
