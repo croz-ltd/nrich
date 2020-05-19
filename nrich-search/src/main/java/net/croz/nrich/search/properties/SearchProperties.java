988570a969e59d8d1a8e90d49d7f7f5b59da4ff6
agrancaric <agrancaric@croz.net>
2020-05-19 15:27:17 +0200

    add todo not to forget moving configuration

--- nrich-search/src/main/java/net/croz/nrich/search/properties/SearchProperties.java
+++ nrich-search/src/main/java/net/croz/nrich/search/properties/SearchProperties.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+// TODO move to search configuration and have a default value
 @Data
 public class SearchProperties {
 
