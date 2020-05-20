f7926f8f797943aa58e052195072b4e0c9c87aa9
agrancaric <agrancaric@croz.net>
2020-05-20 13:39:19 +0200

    remove todo since it seems already decided

--- nrich-search/src/main/java/net/croz/nrich/search/parser/SearchDataParser.java
+++ nrich-search/src/main/java/net/croz/nrich/search/parser/SearchDataParser.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-// TODO decide what should be used as constructor and what should be used as method arguments
 @RequiredArgsConstructor
 @Slf4j
 public class SearchDataParser {
