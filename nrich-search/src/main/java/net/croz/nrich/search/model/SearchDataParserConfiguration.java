6d795c23d6b312d34c85eb5b3dfba348bbafd430
agrancaric <agrancaric@croz.net>
2020-05-22 16:26:41 +0200

    polish remove unused annotations

--- nrich-search/src/main/java/net/croz/nrich/search/model/SearchDataParserConfiguration.java
+++ nrich-search/src/main/java/net/croz/nrich/search/model/SearchDataParserConfiguration.java
@@ -1,25 +1,21 @@
 package net.croz.nrich.search.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @Getter
 public class SearchDataParserConfiguration {
 
-    private boolean resolveFieldMappingUsingPrefix;
+    private final boolean resolveFieldMappingUsingPrefix;
 
-    private List<SearchPropertyMapping> propertyMappingList;
+    private final List<SearchPropertyMapping> propertyMappingList;
 
-    private List<SearchOperatorOverride> searchOperatorOverrideList;
+    private final List<SearchOperatorOverride> searchOperatorOverrideList;
 
-    private SearchFieldConfiguration searchFieldConfiguration;
+    private final SearchFieldConfiguration searchFieldConfiguration;
 
     public static SearchDataParserConfiguration fromSearchConfiguration(final SearchConfiguration<?, ?, ?> searchConfiguration) {
         return new SearchDataParserConfiguration(searchConfiguration.isResolveFieldMappingUsingPrefix(), searchConfiguration.getPropertyMappingList(), searchConfiguration.getSearchOperatorOverrideList(), searchConfiguration.getSearchFieldConfiguration());
