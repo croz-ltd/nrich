e4a9184b5ffce426c23ea99ccf325882b098e493
agrancaric <agrancaric@croz.net>
2020-05-22 14:06:32 +0200

    reformat test

--- nrich-search/src/test/java/net/croz/nrich/search/converter/impl/DefaultStringToTypeConverterTest.java
+++ nrich-search/src/test/java/net/croz/nrich/search/converter/impl/DefaultStringToTypeConverterTest.java
@@ -36,10 +36,22 @@ public class DefaultStringToTypeConverterTest {
 
     private static Stream<Arguments> shouldConvertStringValueToRequiredValueMethodSource() {
         return Stream.of(
-                Arguments.of(null, Boolean.class, null), Arguments.of(null, DefaultStringToTypeConverterTest.class, null), Arguments.of("true", Boolean.class, Boolean.TRUE), Arguments.of("1", Long.class, 1L), Arguments.of("D", Long.class, null), Arguments.of("5", Integer.class, 5),
-                Arguments.of("5", Short.class, Short.valueOf("5")), Arguments.of("ONE", Value.class, Value.ONE), Arguments.of("01.01.1970", Date.class, dateOf("01.01.1970")),
-                Arguments.of("not a date", Date.class, null), Arguments.of("01.01.1970", LocalDate.class, localDateOf("01.01.1970")), Arguments.of("2020-01-01T11:11", LocalDateTime.class, localDateTimeOf("2020-01-01T11:11")),
-                Arguments.of("1.1", BigDecimal.class, new BigDecimal("1.1")), Arguments.of("1.1", Float.class, Double.valueOf("1.1")), Arguments.of("1.1", Double.class, Double.valueOf("1.1")), Arguments.of("nn", Double.class, null)
+                Arguments.of(null, Boolean.class, null),
+                Arguments.of(null, DefaultStringToTypeConverterTest.class, null),
+                Arguments.of("true", Boolean.class, Boolean.TRUE),
+                Arguments.of("1", Long.class, 1L),
+                Arguments.of("D", Long.class, null),
+                Arguments.of("5", Integer.class, 5),
+                Arguments.of("5", Short.class, Short.valueOf("5")),
+                Arguments.of("ONE", Value.class, Value.ONE),
+                Arguments.of("01.01.1970", Date.class, dateOf("01.01.1970")),
+                Arguments.of("not a date", Date.class, null),
+                Arguments.of("01.01.1970", LocalDate.class, localDateOf("01.01.1970")),
+                Arguments.of("2020-01-01T11:11", LocalDateTime.class, localDateTimeOf("2020-01-01T11:11")),
+                Arguments.of("1.1", BigDecimal.class, new BigDecimal("1.1")),
+                Arguments.of("1.1", Float.class, Double.valueOf("1.1")),
+                Arguments.of("1.1", Double.class, Double.valueOf("1.1")),
+                Arguments.of("nn", Double.class, null)
         );
     }
 
