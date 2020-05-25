d0e927119379a7b72e23930fc8b448b8c41beb07
agrancaric <agrancaric@croz.net>
2020-05-25 10:18:11 +0200

    polish, remove unused import

--- nrich-search/src/test/java/net/croz/nrich/search/repository/impl/JpaStringSearchExecutorTest.java
+++ nrich-search/src/test/java/net/croz/nrich/search/repository/impl/JpaStringSearchExecutorTest.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
