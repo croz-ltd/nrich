2203f645374eed789c60976d7eece278cbc303f9
agrancaric <agrancaric@croz.net>
2020-05-27 13:21:55 +0200

    polish, controller will be registered manually

--- nrich-form-configuration/src/main/java/net/croz/nrich/formconfiguration/controller/FormConfigurationController.java
+++ nrich-form-configuration/src/main/java/net/croz/nrich/formconfiguration/controller/FormConfigurationController.java
@@ -7,7 +7,7 @@ import net.croz.nrich.formconfiguration.service.FormConfigurationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,13 +15,13 @@ import java.util.List;
 // TODO decide url format (like this or maybe form-configuration in either case all urls should follow same format?)
 @RequiredArgsConstructor
 @RequestMapping("formConfiguration")
-@RestController
 public class FormConfigurationController {
 
     private final FormConfigurationService formConfigurationService;
 
+    @ResponseBody
     @PostMapping("fetchFormConfiguration")
-    public List<FormConfiguration> fetchFormConfiguration(@Valid @RequestBody final FetchFormConfigurationRequest request) {
+    public List<FormConfiguration> fetchFormConfiguration(@RequestBody @Valid final FetchFormConfigurationRequest request) {
         return formConfigurationService.fetchFormConfigurationList(request);
     }
 }
