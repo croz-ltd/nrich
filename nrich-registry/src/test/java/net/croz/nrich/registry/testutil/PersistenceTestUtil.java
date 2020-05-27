458e13479b1393192df73219e0bed23e59db1a64
agrancaric <agrancaric@croz.net>
2020-05-27 13:50:07 +0200

    allow for excute in transaction to return value

--- nrich-registry/src/test/java/net/croz/nrich/registry/testutil/PersistenceTestUtil.java
+++ nrich-registry/src/test/java/net/croz/nrich/registry/testutil/PersistenceTestUtil.java
@@ -4,6 +4,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
+import java.util.function.Supplier;
 
 public final class PersistenceTestUtil {
 
@@ -15,11 +16,8 @@ public final class PersistenceTestUtil {
         entityManager.clear();
     }
 
-    public static void executeInTransaction(final PlatformTransactionManager transactionManager, final Runnable function) {
-        new TransactionTemplate(transactionManager).execute(status -> {
-            function.run();
-
-            return null;
-        });
+    @SuppressWarnings("unchecked")
+    public static <T> T executeInTransaction(final PlatformTransactionManager transactionManager, final Supplier<?> function) {
+        return (T) new TransactionTemplate(transactionManager).execute(status -> function.get());
     }
 }
