471c1230c9454521f4bf7c3f40d1ec62f63ae730
agrancaric <agrancaric@croz.net>
2020-06-02 15:08:05 +0200

    add limits to page size

--- nrich-registry/src/main/java/net/croz/nrich/registry/data/request/ListRegistryRequest.java
+++ nrich-registry/src/main/java/net/croz/nrich/registry/data/request/ListRegistryRequest.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import net.croz.nrich.registry.data.model.SearchParameter;
 import net.croz.nrich.search.api.model.SortProperty;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -13,9 +15,11 @@ public class ListRegistryRequest {
     @NotNull
     private String classFullName;
 
+    @Min(0)
     @NotNull
     private Integer pageNumber;
 
+    @Max(1000)
     @NotNull
     private Integer pageSize;
 
