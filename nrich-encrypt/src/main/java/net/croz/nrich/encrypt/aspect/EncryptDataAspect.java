e489de6656a9ae6c2f5024268561425734828b50
agrancaric <agrancaric@croz.net>
2020-06-12 13:53:25 +0200

    polish, resolve annotations directly in pointcut

--- nrich-encrypt/src/main/java/net/croz/nrich/encrypt/aspect/EncryptDataAspect.java
+++ nrich-encrypt/src/main/java/net/croz/nrich/encrypt/aspect/EncryptDataAspect.java
@@ -10,10 +10,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 
-import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,29 +20,14 @@ public class EncryptDataAspect extends BaseDataEncryptionAdvice {
 
     private final DataEncryptionService dataEncryptionService;
 
-    /**
-     * Pointcut that matches public methods with {@link DecryptArgument} annotation.
-     */
-    @Pointcut("execution(@net.croz.nrich.encrypt.annotation.DecryptArgument * *(..))")
-    public void decryptAnnotatedMethods() {
-    }
-
-    /**
-     * Pointcut that matches public methods with {@link EncryptResult} annotation.
-     */
-    @Pointcut("execution(@net.croz.nrich.encrypt.annotation.EncryptResult * *(..))")
-    public void encryptAnnotatedMethods() {
-    }
-
-    @Around("decryptAnnotatedMethods()")
-    public Object aroundDecryptAnnotatedMethods(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    @Around("@annotation(annotation)")
+    public Object aroundDecryptAnnotatedMethods(final ProceedingJoinPoint proceedingJoinPoint, final DecryptArgument annotation) throws Throwable {
         final Signature signature = proceedingJoinPoint.getSignature();
-        final DecryptArgument annotation = methodAnnotation(signature, DecryptArgument.class);
 
         final Object[] arguments = proceedingJoinPoint.getArgs();
         final List<Object> argumentList = Arrays.asList(arguments);
 
-        if (annotation != null && annotation.argumentPathList().length > 0) {
+        if (annotation.argumentPathList().length > 0) {
             final EncryptionContext context = EncryptionContext.builder().fullyQualifiedMethodName(methodName(signature)).methodArguments(argumentList).methodDecryptedArguments(argumentList).currentUsername(currentUsername()).build();
             final Object[] decryptedArguments = decryptArguments(context, arguments, Arrays.asList(annotation.argumentPathList()));
 
@@ -55,16 +37,15 @@ public class EncryptDataAspect extends BaseDataEncryptionAdvice {
         return proceedingJoinPoint.proceed(arguments);
     }
 
-    @Around("encryptAnnotatedMethods()")
-    public Object aroundEncryptAnnotatedMethods(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    @Around("@annotation(annotation)")
+    public Object aroundEncryptAnnotatedMethods(final ProceedingJoinPoint proceedingJoinPoint, final EncryptResult annotation) throws Throwable {
         final Signature signature = proceedingJoinPoint.getSignature();
-        final EncryptResult annotation = methodAnnotation(signature, EncryptResult.class);
 
         final Object[] arguments = proceedingJoinPoint.getArgs();
         final List<Object> argumentList = Arrays.asList(arguments);
         Object result = proceedingJoinPoint.proceed(arguments);
 
-        if (annotation != null && annotation.resultPathList().length > 0) {
+        if (annotation.resultPathList().length > 0) {
             final EncryptionContext context = EncryptionContext.builder().fullyQualifiedMethodName(methodName(signature)).methodArguments(argumentList).methodDecryptedArguments(argumentList).currentUsername(currentUsername()).build();
 
             result = encryptResult(context, result, Arrays.asList(annotation.resultPathList()));
@@ -78,14 +59,6 @@ public class EncryptDataAspect extends BaseDataEncryptionAdvice {
         return dataEncryptionService;
     }
 
-    private <T extends Annotation> T methodAnnotation(final Signature signature, final Class<T> annotationType) {
-        T annotation = null;
-        if (signature instanceof MethodSignature) {
-            annotation = ((MethodSignature) signature).getMethod().getAnnotation(annotationType);
-        }
-
-        return annotation;
-    }
 
     private String methodName(final Signature signature) {
         return String.format(EncryptConstants.METHOD_NAME_FORMAT, signature.getDeclaringType().getName(), signature.getName());
