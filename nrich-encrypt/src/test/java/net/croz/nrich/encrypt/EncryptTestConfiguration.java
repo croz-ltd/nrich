984e14431a4a59ea73ce34d170a8e6e8421e1237
agrancaric <agrancaric@croz.net>
2020-06-12 11:23:55 +0200

    add test configuration and first test for aes encryption service

---
+++ nrich-encrypt/src/test/java/net/croz/nrich/encrypt/EncryptTestConfiguration.java
@@ -0,0 +1,34 @@
+package net.croz.nrich.encrypt;
+
+import net.croz.nrich.encrypt.aspect.EncryptDataAspect;
+import net.croz.nrich.encrypt.service.DataEncryptionService;
+import net.croz.nrich.encrypt.service.TextEncryptionService;
+import net.croz.nrich.encrypt.service.aes.AesTextEncryptionService;
+import net.croz.nrich.encrypt.service.impl.DataEncryptionServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+
+@Configuration(proxyBeanMethods = false)
+public class EncryptTestConfiguration {
+
+    @Bean
+    public TextEncryptionService textEncryptionService() {
+        final BytesEncryptor encryptor = Encryptors.standard(KeyGenerators.string().generateKey(), KeyGenerators.string().generateKey());
+
+        return new AesTextEncryptionService(encryptor, "UTF-8");
+    }
+
+    @Bean
+    public DataEncryptionService dataEncryptionService(final TextEncryptionService textEncryptionService) {
+        return new DataEncryptionServiceImpl(textEncryptionService);
+    }
+
+    @Bean
+    public EncryptDataAspect encryptDataAspect(final DataEncryptionService dataEncryptionService) {
+        return new EncryptDataAspect(dataEncryptionService);
+    }
+
+}
