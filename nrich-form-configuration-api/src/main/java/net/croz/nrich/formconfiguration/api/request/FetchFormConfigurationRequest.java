3d07ac0f207db7ba36c32bc86b670df85a004ecf
agrancaric <agrancaric@croz.net>
2020-06-26 13:16:07 +0200

    fix constraint for request

--- nrich-form-configuration-api/src/main/java/net/croz/nrich/formconfiguration/api/request/FetchFormConfigurationRequest.java
+++ nrich-form-configuration-api/src/main/java/net/croz/nrich/formconfiguration/api/request/FetchFormConfigurationRequest.java
@@ -4,8 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Setter
@@ -14,7 +14,7 @@ import java.util.List;
 public class FetchFormConfigurationRequest {
 
     @NotNull
-    @Min(value = 1)
+    @Size(min = 1)
     private List<String> formIdList;
 
 }
