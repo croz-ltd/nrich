b382067f4d08f44c7cc643838f23e321e42cffd6
agrancaric <agrancaric@croz.net>
2020-06-29 12:29:42 +0200

    add constraints to page number and size

--- nrich-search-api/src/main/java/net/croz/nrich/search/api/request/BaseSortablePageableRequest.java
+++ nrich-search-api/src/main/java/net/croz/nrich/search/api/request/BaseSortablePageableRequest.java
@@ -4,14 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import net.croz.nrich.search.api.model.sort.SortProperty;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Setter
 @Getter
 public abstract class BaseSortablePageableRequest implements SortablePageableRequest {
 
+    @Min(1)
+    @NotNull
     private Integer pageSize;
 
+    @Min(0)
+    @NotNull
     private Integer pageNumber;
 
     private List<SortProperty> sortPropertyList;
