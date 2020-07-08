d2b3a8a1530516fc1deca31b3f9e4c5613044f12
agrancaric <agrancaric@croz.net>
2020-07-08 13:33:38 +0200

    move all history constants to envers constants since they are used in both places

--- nrich-registry/src/main/java/net/croz/nrich/registry/core/constants/RegistryEnversConstants.java
+++ nrich-registry/src/main/java/net/croz/nrich/registry/core/constants/RegistryEnversConstants.java
@@ -10,9 +10,15 @@ public final class RegistryEnversConstants {
 
     public static final String ENVERS_REVISION_NUMBER_ANNOTATION = "org.hibernate.envers.RevisionNumber";
 
-    public static final String REVISION_NUMBER_PROPERTY_NAME = "id";
+    public static final String REVISION_TIMESTAMP_PROPERTY_NAME = "revisionTimestamp";
 
-    public static final String REVISION_TIMESTAMP_PROPERTY_NAME = "timestamp";
+    public static final String REVISION_NUMBER_PROPERTY_NAME = "revisionNumber";
+
+    public static final String REVISION_TIMESTAMP_PROPERTY_DEFAULT_NAME = "timestamp";
+
+    public static final String REVISION_NUMBER_PROPERTY_DEFAULT_NAME = "id";
+
+    public static final String REVISION_TYPE_PROPERTY_NAME = "revisionType";
 
     private RegistryEnversConstants() {
     }
