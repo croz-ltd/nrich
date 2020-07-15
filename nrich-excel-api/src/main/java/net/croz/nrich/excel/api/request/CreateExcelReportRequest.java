7dea7d4580d180ed599b6aa9b7a9821158fa7a4c
agrancaric <agrancaric@croz.net>
2020-07-15 14:37:44 +0200

    add javadoc for CreateExcelReportRequest

--- nrich-excel-api/src/main/java/net/croz/nrich/excel/api/request/CreateExcelReportRequest.java
+++ nrich-excel-api/src/main/java/net/croz/nrich/excel/api/request/CreateExcelReportRequest.java
@@ -13,18 +13,39 @@ import java.util.List;
 @Builder
 public class CreateExcelReportRequest {
 
+    /**
+     * File where report will be written to.
+     */
     private final File outputFile;
 
+    /**
+     * Path to template (template is resolved from this path using Springs ResourceLoader)
+     */
     private final String templatePath;
 
+    /**
+     * A list of {@link TemplateVariable} instances that will be used to fill variables defined in the template.
+     */
     private final List<TemplateVariable> templateVariableList;
 
+    /**
+     * A list of {@link ColumnDataFormat} instances that allow for overriding of data format for specific columns.
+     */
     private final List<ColumnDataFormat> columnDataFormatList;
 
+    /**
+     * Row index from which data should be written to report (if for example template holds column headers in first couple of rows).
+     */
     private final int firstRowIndex;
 
+    /**
+     *   Batch size used for data fetch from {@link MultiRowDataProvider}.
+     */
     private final int batchSize;
 
+    /**
+     * Interface that is used for fetching data, it should return multiple row data.
+     */
     private final MultiRowDataProvider multiRowDataProvider;
 
 }
