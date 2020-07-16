6966187665ed537a6dfaaf09cb81b623488f732c
agrancaric <agrancaric@croz.net>
2020-07-16 08:39:30 +0200

    add TODO not to forget

--- nrich-excel-api/src/main/java/net/croz/nrich/excel/api/converter/CellValueConverter.java
+++ nrich-excel-api/src/main/java/net/croz/nrich/excel/api/converter/CellValueConverter.java
@@ -11,5 +11,6 @@ public interface CellValueConverter {
 
     boolean supports(CellHolder cell, Object value);
 
+    // TODO this doesn't seem to belong here, move to separate class
     List<TypeDataFormat> typeDataFormatList();
 }
