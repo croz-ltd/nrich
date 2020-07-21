ac9e12aaab4a1bd905c36e6dd5c968b82aec8c33
agrancaric <agrancaric@croz.net>
2020-07-21 13:59:09 +0200

    add all of user defined TypeDataFormat list (not just those that override existing formats)

--- nrich-excel/src/main/java/net/croz/nrich/excel/util/TypeDataFormatUtil.java
+++ nrich-excel/src/main/java/net/croz/nrich/excel/util/TypeDataFormatUtil.java
@@ -3,6 +3,7 @@ package net.croz.nrich.excel.util;
 import net.croz.nrich.excel.api.model.TypeDataFormat;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,14 +15,17 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class TypeDataFormatUtil {
 
     private TypeDataFormatUtil() {
     }
 
-    public static List<TypeDataFormat> resolveTypeDataFormatList(final String dateFormat, final String dateTimeFormat, final String integerNumberFormat, final String decimalNumberFormat, final boolean writeDateWithTime, final List<TypeDataFormat> overriddenTypeDataFormatList) {
+    public static List<TypeDataFormat> resolveTypeDataFormatList(final String dateFormat, final String dateTimeFormat, final String integerNumberFormat, final String decimalNumberFormat, final boolean writeDateWithTime, final List<TypeDataFormat> additionalTypeDataFormatList) {
         final String resolvedDateTimeFormat = writeDateWithTime ? dateTimeFormat : dateFormat;
+
+        final List<TypeDataFormat> nonNullAdditionalDataFormatList = Optional.ofNullable(additionalTypeDataFormatList).orElse(Collections.emptyList());
 
         final List<TypeDataFormat> typeDataFormatList = Arrays.asList(
                 new TypeDataFormat(Date.class, dateFormat),
@@ -33,18 +37,25 @@ public final class TypeDataFormatUtil {
                 new TypeDataFormat(Short.class, integerNumberFormat),
                 new TypeDataFormat(Integer.class, integerNumberFormat),
                 new TypeDataFormat(Long.class, integerNumberFormat),
-                new TypeDataFormat(BigDecimal.class, decimalNumberFormat),
+                new TypeDataFormat(BigInteger.class, integerNumberFormat),
                 new TypeDataFormat(Float.class, decimalNumberFormat),
-                new TypeDataFormat(Double.class, decimalNumberFormat)
+                new TypeDataFormat(Double.class, decimalNumberFormat),
+                new TypeDataFormat(BigDecimal.class, decimalNumberFormat)
         );
 
-        return typeDataFormatList.stream()
-                .map(typeDataFormat -> Optional.ofNullable(findOverriddenFormatForType(overriddenTypeDataFormatList, typeDataFormat.getType())).orElse(typeDataFormat))
+        final List<TypeDataFormat> allTypeDataFormatList = typeDataFormatList.stream()
+                .map(typeDataFormat -> Optional.ofNullable(findTypeDataFormat(nonNullAdditionalDataFormatList, typeDataFormat.getType())).orElse(typeDataFormat))
                 .collect(Collectors.toList());
+
+        final List<TypeDataFormat> notAddedOverriddenFormatList = nonNullAdditionalDataFormatList.stream()
+                .filter(typeDataFormat -> findTypeDataFormat(allTypeDataFormatList, typeDataFormat.getType()) == null)
+                .collect(Collectors.toList());
+
+        return Stream.concat(allTypeDataFormatList.stream(), notAddedOverriddenFormatList.stream()).collect(Collectors.toList());
     }
 
-    private static TypeDataFormat findOverriddenFormatForType(final List<TypeDataFormat> overriddenTypeDataFormatList, final Class<?> type) {
-        return Optional.ofNullable(overriddenTypeDataFormatList).orElse(Collections.emptyList()).stream()
+    private static TypeDataFormat findTypeDataFormat(final List<TypeDataFormat> typeDataFormatList, final Class<?> type) {
+        return typeDataFormatList.stream()
                 .filter(typeDataFormat -> type.equals(typeDataFormat.getType()))
                 .findFirst()
                 .orElse(null);
