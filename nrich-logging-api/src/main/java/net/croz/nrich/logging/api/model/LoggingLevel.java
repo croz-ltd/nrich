27ae7da4a8abcdfd956ccae0b67aa419f4e284d6
agrancaric <agrancaric@croz.net>
2020-07-22 13:02:15 +0200

    add javadoc for logging library

--- nrich-logging-api/src/main/java/net/croz/nrich/logging/api/model/LoggingLevel.java
+++ nrich-logging-api/src/main/java/net/croz/nrich/logging/api/model/LoggingLevel.java
@@ -1,5 +1,8 @@
 package net.croz.nrich.logging.api.model;
 
+/**
+ * Level on which exceptions should be logged (default is ERROR unless configured otherwise).
+ */
 public enum LoggingLevel {
 
     DEBUG, WARN, INFO, ERROR
--- nrich-logging-api/src/main/java/net/croz/nrich/logging/api/service/LoggingService.java
+++ nrich-logging-api/src/main/java/net/croz/nrich/logging/api/service/LoggingService.java
@@ -2,13 +2,40 @@ package net.croz.nrich.logging.api.service;
 
 import java.util.Map;
 
+/**
+ * Performs logging of exceptions at specified verbosity level and logging level (can be configured for each exception).
+ */
 public interface LoggingService {
 
+    /**
+     * Logs exception. Exception verbosity and logging level are resolved from configuration. If no exception or verbosity level is defined defaults are used (LoggingLevel.ERROR and LoggingVerbosityLevel.FULL).
+     *
+     * @param exception exception to log
+     * @param exceptionAuxiliaryData additional data that should be logged
+     */
     void logInternalException(Exception exception, Map<String, ?> exceptionAuxiliaryData);
 
+    /**
+     * Logs exception on compact verbosity level. Logging level is resolved from configuration for exception or LoggingLevel.ERROR is used.
+     *
+     * @param exception exception to log
+     * @param exceptionAuxiliaryData additional data that should be logged
+     */
     void logInternalExceptionAtCompactVerbosityLevel(Exception exception, Map<String, ?> exceptionAuxiliaryData);
 
+    /**
+     * Logs exception on full verbosity level. Logging level is resolved from configuration for exception or LoggingLevel.ERROR is used.
+     *
+     * @param exception exception to log
+     * @param exceptionAuxiliaryData additional data that should be logged
+     */
     void logInternalExceptionAtFullVerbosityLevel(Exception exception, Map<String, ?> exceptionAuxiliaryData);
 
+    /**
+     *  Logs external exception (i.e. exceptions for which a class is not on class path)
+     * @param exceptionClassName class name of exception
+     * @param exceptionMessage exception message
+     * @param exceptionAuxiliaryData additional data that should be logged
+     */
     void logExternalException(String exceptionClassName, String exceptionMessage, Map<String, ?> exceptionAuxiliaryData);
 }
