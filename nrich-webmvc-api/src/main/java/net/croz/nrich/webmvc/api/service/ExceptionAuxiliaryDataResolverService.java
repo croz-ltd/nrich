996234162064b3688c2ea93949c00061f563cbef
agrancaric <agrancaric@croz.net>
2020-07-23 11:27:06 +0200

    add javadoc for webmvc api

--- nrich-webmvc-api/src/main/java/net/croz/nrich/webmvc/api/service/ExceptionAuxiliaryDataResolverService.java
+++ nrich-webmvc-api/src/main/java/net/croz/nrich/webmvc/api/service/ExceptionAuxiliaryDataResolverService.java
@@ -3,8 +3,18 @@ package net.croz.nrich.webmvc.api.service;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+/**
+ * Resolves auxiliary data for exception (i.e UUID, request uri etc)
+ */
 public interface ExceptionAuxiliaryDataResolverService {
 
+    /**
+     * Returns map containing auxiliary data for exception and request.
+     *
+     * @param exception for which to resolve auxiliary data
+     * @param request current http request
+     * @return map of auxiliary data
+     */
     Map<String, Object> resolveRequestExceptionAuxiliaryData(Exception exception, HttpServletRequest request);
 
 }
--- nrich-webmvc-api/src/main/java/net/croz/nrich/webmvc/api/service/ExceptionHttpStatusResolverService.java
+++ nrich-webmvc-api/src/main/java/net/croz/nrich/webmvc/api/service/ExceptionHttpStatusResolverService.java
@@ -1,8 +1,17 @@
 package net.croz.nrich.webmvc.api.service;
 
 // TODO for now returning Integer instead of Springs HttpStatus to avoid dependency on spring-web, not sure about that decision
+/**
+ * Resolve http status for exception. If invalid status value is returned status 500 is used.
+ */
 public interface ExceptionHttpStatusResolverService {
 
+    /**
+     * Returns http status value.
+     *
+     * @param exception exception to resolve status for
+     * @return status value
+     */
     Integer resolveHttpStatusForException(Exception exception);
 
 }
