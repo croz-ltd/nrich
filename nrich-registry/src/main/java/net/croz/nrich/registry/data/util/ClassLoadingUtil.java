0f46f55a84d9b2057497f034b40b6a037bd83255
agrancaric <agrancaric@croz.net>
2020-07-24 12:08:38 +0200

    polish

--- nrich-registry/src/main/java/net/croz/nrich/registry/data/util/ClassLoadingUtil.java
+++ nrich-registry/src/main/java/net/croz/nrich/registry/data/util/ClassLoadingUtil.java
@@ -8,10 +8,12 @@ public final class ClassLoadingUtil {
     }
 
     public static Class<?> loadClassFromList(final List<String> classNameList) {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
         Class<?> foundClass = null;
         for (final String className : classNameList) {
             try {
-                foundClass = Class.forName(className, true, Thread.currentThread().getContextClassLoader());
+                foundClass = Class.forName(className, true, classLoader);
             }
             catch (final Exception ignored) {
             }
