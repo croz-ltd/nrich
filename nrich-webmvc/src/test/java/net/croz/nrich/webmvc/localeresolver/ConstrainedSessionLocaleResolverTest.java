d3b720a9f850a85d801cacbab78c006b92089aa9
agrancaric <agrancaric@croz.net>
2022-02-25 12:57:56 +0100

    Add additional tests for ConstrainedLocalSessionResolver

--- nrich-webmvc/src/test/java/net/croz/nrich/webmvc/localeresolver/ConstrainedSessionLocaleResolverTest.java
+++ nrich-webmvc/src/test/java/net/croz/nrich/webmvc/localeresolver/ConstrainedSessionLocaleResolverTest.java
@@ -16,12 +16,10 @@ class ConstrainedSessionLocaleResolverTest {
     @Test
     void shouldOnlyAllowSettingOfAllowedLocale() {
         // given
-        MockHttpSession session = new MockHttpSession();
-        MockHttpServletRequest request = new MockHttpServletRequest();
-
-        request.setSession(session);
-
         ConstrainedSessionLocaleResolver constrainedSessionLocaleResolver = new ConstrainedSessionLocaleResolver("hr", Arrays.asList("hr", "en"));
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(new MockHttpSession());
 
         // when
         constrainedSessionLocaleResolver.setLocale(request, new MockHttpServletResponse(), Locale.CHINA);
@@ -35,5 +33,23 @@ class ConstrainedSessionLocaleResolverTest {
 
         // then
         assertThat(request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)).isEqualTo(Locale.ENGLISH);
+    }
+
+    @Test
+    void shouldSetDefaultLocaleWhenSettingNullLocale() {
+        // given
+        ConstrainedSessionLocaleResolver constrainedSessionLocaleResolver = new ConstrainedSessionLocaleResolver(null, Arrays.asList("hr", "zh_CN", "en"));
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        request.setSession(new MockHttpSession());
+        Locale.setDefault(Locale.UK);
+
+        // when
+        constrainedSessionLocaleResolver.setLocale(request, new MockHttpServletResponse(), null);
+
+        // then
+        assertThat(request.getSession()).isNotNull();
+        assertThat(request.getSession().getAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME)).isEqualTo(Locale.UK);
     }
 }
