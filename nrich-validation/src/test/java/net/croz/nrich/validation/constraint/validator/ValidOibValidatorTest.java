e8f11dde8245ac4c0bc128c55058da37af46b55d
agrancaric <agrancaric@croz.net>
2022-02-25 12:48:08 +0100

    Add additional tests for OibValidator

--- nrich-validation/src/test/java/net/croz/nrich/validation/constraint/validator/ValidOibValidatorTest.java
+++ nrich-validation/src/test/java/net/croz/nrich/validation/constraint/validator/ValidOibValidatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -29,11 +30,9 @@ class ValidOibValidatorTest {
         assertThat(result).isTrue();
     }
 
-    @Test
-    void shouldReturnFalseForInvalidOib() {
-        // given
-        String oib = "11111111110";
-
+    @ValueSource(strings = { "aaa", "11111111110" })
+    @ParameterizedTest
+    void shouldReturnFalseForInvalidOib(String oib) {
         // when
         boolean result = validOibValidator.isValid(oib, constraintValidatorContext);
 
