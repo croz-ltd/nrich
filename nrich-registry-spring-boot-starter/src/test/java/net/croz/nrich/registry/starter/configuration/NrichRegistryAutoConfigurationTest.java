e297f7c52a9b287f1c8a945e5e8e8147b941f2ea
agrancaric <agrancaric@croz.net>
2022-02-25 11:16:41 +0100

    Add additional tests for NrichRegistryAutoConfiguration to verify envers gets registered when present and that registryDataModelMapper skips id copying

--- nrich-registry-spring-boot-starter/src/test/java/net/croz/nrich/registry/starter/configuration/NrichRegistryAutoConfigurationTest.java
+++ nrich-registry-spring-boot-starter/src/test/java/net/croz/nrich/registry/starter/configuration/NrichRegistryAutoConfigurationTest.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import net.croz.nrich.registry.api.configuration.service.RegistryConfigurationService;
 import net.croz.nrich.registry.api.data.service.RegistryDataFormConfigurationResolverService;
 import net.croz.nrich.registry.api.data.service.RegistryDataService;
+import net.croz.nrich.registry.api.history.service.RegistryHistoryService;
 import net.croz.nrich.registry.configuration.controller.RegistryConfigurationController;
 import net.croz.nrich.registry.core.service.RegistryConfigurationResolverService;
 import net.croz.nrich.registry.data.controller.RegistryDataController;
 import net.croz.nrich.registry.data.service.RegistryDataRequestConversionService;
+import net.croz.nrich.registry.history.controller.RegistryHistoryController;
 import net.croz.nrich.registry.security.interceptor.RegistryConfigurationUpdateInterceptor;
+import net.croz.nrich.registry.starter.configuration.stub.ModelMapperTestEntity;
 import net.croz.nrich.registry.starter.configuration.stub.RegistryUserConfiguration;
 import net.croz.nrich.registry.starter.configuration.stub.RegistryUserFormConfiguration;
 import net.croz.nrich.search.api.converter.StringToEntityPropertyMapConverter;
 import net.croz.nrich.search.api.converter.StringToTypeConverter;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -44,10 +48,12 @@ class NrichRegistryAutoConfigurationTest {
             assertThat(context).hasSingleBean(RegistryConfigurationService.class);
             assertThat(context).hasSingleBean(RegistryDataService.class);
             assertThat(context).hasSingleBean(RegistryDataRequestConversionService.class);
+            assertThat(context).hasSingleBean(RegistryHistoryService.class);
 
             assertThat(context).doesNotHaveBean(RegistryConfigurationController.class);
             assertThat(context).doesNotHaveBean(RegistryDataController.class);
             assertThat(context).doesNotHaveBean(RegistryDataFormConfigurationResolverService.class);
+            assertThat(context).doesNotHaveBean(RegistryHistoryController.class);
         });
     }
 
@@ -57,6 +63,7 @@ class NrichRegistryAutoConfigurationTest {
         webContextRunner.withUserConfiguration(RegistryUserConfiguration.class).withBean(LocalValidatorFactoryBean.class).run(context -> {
             assertThat(context).hasSingleBean(RegistryConfigurationController.class);
             assertThat(context).hasSingleBean(RegistryDataController.class);
+            assertThat(context).hasSingleBean(RegistryHistoryController.class);
         });
     }
 
@@ -70,5 +77,26 @@ class NrichRegistryAutoConfigurationTest {
     @Test
     void shouldNotCreateDefaultValueConverterWhenCreationIsDisabled() {
         contextRunner.withPropertyValues("nrich.registry.default-converter-enabled=false").run(context -> assertThat(context).doesNotHaveBean(StringToTypeConverter.class));
+    }
+
+    @Test
+    void shouldSkipIdCopyInRegisteredModelMapperInstance() {
+        contextRunner.withUserConfiguration(RegistryUserConfiguration.class).withBean(LocalValidatorFactoryBean.class).run(context -> {
+            // given
+            ModelMapper modelMapper = context.getBean("registryDataModelMapper", ModelMapper.class);
+            ModelMapperTestEntity modelMapperTestEntity = new ModelMapperTestEntity();
+
+            modelMapperTestEntity.setName("name");
+            modelMapperTestEntity.setId("id");
+
+            // when
+            ModelMapperTestEntity result = new ModelMapperTestEntity();
+            modelMapper.map(modelMapperTestEntity, result);
+
+            // then
+            assertThat(result.getId()).isEqualTo("initial");
+            assertThat(result.getName()).isEqualTo("name");
+
+        });
     }
 }
---
+++ nrich-registry-spring-boot-starter/src/test/java/net/croz/nrich/registry/starter/configuration/stub/ModelMapperTestEntity.java
@@ -0,0 +1,14 @@
+package net.croz.nrich.registry.starter.configuration.stub;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ModelMapperTestEntity {
+
+    private String id = "initial";
+
+    private String name = "initial";
+
+}
