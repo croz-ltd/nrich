cfd1e2144eb1d4fda7bd6e06f86b55be6d01ea76
agrancaric <agrancaric@croz.net>
2022-02-25 12:57:32 +0100

    Add test for ControllerRegistrationAdvice and avoid calling getTarget if registration has been disabled

--- nrich-webmvc/src/test/java/net/croz/nrich/webmvc/advice/ControllerEditorRegistrationAdviceTest.java
+++ nrich-webmvc/src/test/java/net/croz/nrich/webmvc/advice/ControllerEditorRegistrationAdviceTest.java
@@ -2,11 +2,27 @@ package net.croz.nrich.webmvc.advice;
 
 import net.croz.nrich.webmvc.test.BaseWebTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.WebDataBinder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 class ControllerEditorRegistrationAdviceTest extends BaseWebTest {
+
+    @Test
+    void shouldNotRegisterBindersWhenDisabled() {
+        // given
+        WebDataBinder webDataBinder = mock(WebDataBinder.class);
+        ControllerEditorRegistrationAdvice controllerEditorRegistrationAdvice = new ControllerEditorRegistrationAdvice(false, false, null);
+
+        // when
+        controllerEditorRegistrationAdvice.initBinder(webDataBinder);
+
+        // then
+        verifyNoInteractions(webDataBinder);
+    }
 
     @Test
     void shouldConvertEmptyStringsToNull() throws Exception {
@@ -28,5 +44,4 @@ class ControllerEditorRegistrationAdviceTest extends BaseWebTest {
         // then
         assertThat(response).isEqualTo("value=nonTransient transientValue=null");
     }
-
 }
