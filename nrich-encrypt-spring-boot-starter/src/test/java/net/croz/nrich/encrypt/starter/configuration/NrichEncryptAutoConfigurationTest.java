48c142e7c2c8985cef22f7f69a4428bae9f89a6e
agrancaric <agrancaric@croz.net>
2022-02-25 10:46:16 +0100

    Add missing tests for creation of TextEncryptionService in  NrichEncryptAutoConfiguration

--- nrich-encrypt-spring-boot-starter/src/test/java/net/croz/nrich/encrypt/starter/configuration/NrichEncryptAutoConfigurationTest.java
+++ nrich-encrypt-spring-boot-starter/src/test/java/net/croz/nrich/encrypt/starter/configuration/NrichEncryptAutoConfigurationTest.java
@@ -4,6 +4,8 @@ import net.croz.nrich.encrypt.api.service.DataEncryptionService;
 import net.croz.nrich.encrypt.api.service.TextEncryptionService;
 import net.croz.nrich.encrypt.aspect.EncryptDataAspect;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
@@ -36,5 +38,12 @@ class NrichEncryptAutoConfigurationTest {
                 "nrich.encrypt.encryption-configuration-list[0].method-to-encrypt-decrypt=methodToEncrypt",
                 "nrich.encrypt.encryption-configuration-list[0].property-to-encrypt-decrypt-list=property",
                 "nrich.encrypt.encryption-configuration-list[0].encryption-operation=ENCRYPT").run(context -> assertThat(context).hasBean("encryptAdvisor"));
+    }
+
+    @CsvSource({ "'',''", "ec49c585b7c44f2b,''", "'',ec49c585b7c44f2b" })
+    @ParameterizedTest
+    void shouldCreateTextEncryptionServiceWithProvidedOrDefaultPasswordAndSalt(String password, String salt) {
+        // expect
+        contextRunner.withPropertyValues("nrich.encrypt.encrypt-password=" + password, "nrich.encrypt.encrypt-salt=" + salt).run(context -> assertThat(context).hasSingleBean(TextEncryptionService.class));
     }
 }
