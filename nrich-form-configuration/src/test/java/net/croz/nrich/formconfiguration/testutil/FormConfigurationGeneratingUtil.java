c7022c5e1a4b25451384128dc06a9509098a0a0e
agrancaric <agrancaric@croz.net>
2022-03-15 12:25:37 +0100

    Add missing test for FormConfigurationController

--- nrich-form-configuration/src/test/java/net/croz/nrich/formconfiguration/testutil/FormConfigurationGeneratingUtil.java
+++ nrich-form-configuration/src/test/java/net/croz/nrich/formconfiguration/testutil/FormConfigurationGeneratingUtil.java
@@ -1,10 +1,13 @@
 package net.croz.nrich.formconfiguration.testutil;
 
 import net.croz.nrich.formconfiguration.api.model.ConstrainedProperty;
+import net.croz.nrich.formconfiguration.api.model.FormConfiguration;
+import net.croz.nrich.formconfiguration.api.request.FetchFormConfigurationRequest;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.metadata.ConstraintDescriptor;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
@@ -38,5 +41,17 @@ public final class FormConfigurationGeneratingUtil {
             .type(String.class)
             .parentType(parentType).build();
 
+    }
+
+    public static FetchFormConfigurationRequest createFetchFormConfigurationRequest() {
+        FetchFormConfigurationRequest request = new FetchFormConfigurationRequest();
+
+        request.setFormIdList(Arrays.asList("formId1", "formId2"));
+
+        return request;
+    }
+
+    public static FormConfiguration createFormConfiguration() {
+        return new FormConfiguration("formId1", Collections.emptyList());
     }
 }
