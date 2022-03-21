75d95c2d3496af6f4eb4e03fd98c2bbf7e9b42a8
agrancaric <agrancaric@croz.net>
2022-03-21 10:14:06 +0100

    Rename nrich-spring-util module to nrich-spring

--- nrich-spring/src/test/java/net/croz/nrich/spring/SpringTestConfiguration.java
+++ nrich-spring/src/test/java/net/croz/nrich/spring/SpringTestConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-public class SpringUtilTestConfiguration {
+public class SpringTestConfiguration {
 
     @Bean
     public ApplicationContextHolder applicationContextHolder() {
