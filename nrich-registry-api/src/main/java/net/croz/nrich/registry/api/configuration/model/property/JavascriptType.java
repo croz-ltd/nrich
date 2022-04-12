db826c5a3607aad8d15aeda45120b2206b8e0b64
agrancaric <agrancaric@croz.net>
2022-04-12 08:48:27 +0200

    Add @JsonValue annotation to serialize JavascriptType as lowercase

--- nrich-registry-api/src/main/java/net/croz/nrich/registry/api/configuration/model/property/JavascriptType.java
+++ nrich-registry-api/src/main/java/net/croz/nrich/registry/api/configuration/model/property/JavascriptType.java
@@ -1,9 +1,18 @@
 package net.croz.nrich.registry.api.configuration.model.property;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Locale;
+
 /**
  * Enum representing Javascript type, even though date is not a type it is added for easier handling on client.
  */
 public enum JavascriptType {
 
-    STRING, BOOLEAN, NUMBER, DATE, OBJECT
+    STRING, BOOLEAN, NUMBER, DATE, OBJECT;
+
+    @JsonValue
+    public String getName() {
+        return name().toLowerCase(Locale.ROOT);
+    }
 }
