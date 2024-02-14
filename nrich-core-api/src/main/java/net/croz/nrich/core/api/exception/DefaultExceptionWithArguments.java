c50b1264c7a80da6c27ba2b5d198a90bfebd67df
agrancaric <agrancaric@croz.net>
2024-02-14 08:55:37 +0100

    Add DefaultExceptionWithArguments class for easier usage of ExceptionWithArguments interface

---
+++ nrich-core-api/src/main/java/net/croz/nrich/core/api/exception/DefaultExceptionWithArguments.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2020-2023 CROZ d.o.o, the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package net.croz.nrich.core.api.exception;
+
+/**
+ * Default implementation of {@link ExceptionWithArguments} interface.
+ */
+public class DefaultExceptionWithArguments extends RuntimeException implements ExceptionWithArguments {
+
+    private final transient Object[] argumentList;
+
+    DefaultExceptionWithArguments(String message, Object[] argumentList) {
+        super(message);
+        this.argumentList = argumentList;
+    }
+
+    @Override
+    public Object[] getArgumentList() {
+        return argumentList;
+    }
+}
